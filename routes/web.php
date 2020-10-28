17adf368f97249c830eee0589d75df403422747e
Sheila Putri Javana <jvnsheila@gmail.com>
2020-10-28 15:06:28 +0700

    Remove example routes

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','HomeController@index');
 
-Route::get('/contoh','HomeController@contoh');
-
-Route::post('/contohPost', 'HomeController@contohPost');
-
 Route::get('/tentang', function () {
     return view('tentang');
 });
