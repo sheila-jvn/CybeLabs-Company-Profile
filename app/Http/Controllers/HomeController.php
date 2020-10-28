f77a4ad78096035d389514d5aadd87f8cb9bec7f
Sheila Putri Javana <jvnsheila@gmail.com>
2020-10-28 15:04:48 +0700

    Remove example functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,24 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Siswa;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index (){
-        return view('index');
-    }
-
-    public function contoh () {
-        return view('contoh');
-    }
-
-    public function contohPost(Request $request)
+    public function index()
     {
-        $siswa = new Siswa();
-        $siswa->nama = 'Sheila';
-   
-        $siswa->save();
+        return view('index');
     }
 }
